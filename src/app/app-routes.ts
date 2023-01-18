71aa34d990f1b9d136c005006c04c1eae9ce6275
Tibor Krajčovič <tibor.krajcovic@softec.sk>
2023-01-18 09:33:49 +0100

    scanner removed

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,11 +2,9 @@ import { Routes } from "@angular/router";
 import { AboutComponent } from "./views/about/about.component";
 import { HomeComponent } from "./views/home/home.component";
 import { UserProfileComponent } from "./views/user-profile/user-profile.component";
-import { ScannerComponent } from "./views/scanner/scanner.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent, title: 'Home' },
   { path: 'profile', component: UserProfileComponent, title: 'Profile' },
-  { path: 'scanner', component: ScannerComponent, title: 'Scanner' },
   { path: 'about', component: AboutComponent, title: 'About' },
 ];
